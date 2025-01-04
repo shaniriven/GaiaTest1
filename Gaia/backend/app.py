638270ba2ab995a816fc09512d12acd567d49c9a
DorYubiler <DorYubiler@gmail.com>
2025-01-04 16:37:11 +0200

    backend

---
+++ Gaia/backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return "<p>Hello<p>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
